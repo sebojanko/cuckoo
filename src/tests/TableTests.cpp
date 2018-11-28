0f69ac9ab18e2fdaff08c81c1b52f96a946cd54d
Sebastian Janko <janko.sebastian@gmail.com>
2018-11-28 22:56:24 +0100

    fixed tests

--- src/tests/TableTests.cpp
+++ src/tests/TableTests.cpp
@@ -6,55 +6,55 @@
 void test_Table__InsertedOneElement__FindsOneElement() {
     Hasher hasher(8);
     Table t = Table(&hasher);
-    t.Insert(1);
-    assert(t.Contains(1));
+    t.Insert("1");
+    assert(t.Contains("1"));
     std::cout << "PASS test_Table__InsertedOneElement__FindsOneElement" << std::endl;
 }
 
 void test_Table__InsertedOneElement__SearchForAnother() {
     Hasher hasher(8);
     Table t = Table(&hasher);
-    t.Insert(1);
-    assert(!t.Contains(2));
+    t.Insert("1");
+    assert(!t.Contains("2"));
     std::cout << "PASS test_Table__InsertedOneElement__SearchForAnother" << std::endl;
 }
 
 void test_Table__InsertedTwoElements__FindsBoth() {
     Hasher hasher(8);
     Table t = Table(&hasher);
-    t.Insert(1);
-    t.Insert(2);
-    assert(t.Contains(1));
-    assert(t.Contains(2));
+    t.Insert("1");
+    t.Insert("2");
+    assert(t.Contains("1"));
+    assert(t.Contains("2"));
     std::cout << "PASS test_Table__InsertedTwoElements__FindsBoth" << std::endl;
 }
 
 void test_Table__InsertedOneElement__DeletesOneElement() {
     Hasher hasher(8);
     Table t = Table(&hasher);
-    t.Insert(1);
-    assert(t.Remove(1));
-    assert(!t.Contains(1));
+    t.Insert("1");
+    assert(t.Remove("1"));
+    assert(!t.Contains("1"));
     std::cout << "PASS test_Table__InsertedOneElement__DeletesOneElement" << std::endl;
 }
 
 void test_Table__InsertedOneElement__UnsuccessfulDeletionOfNonExistingElement() {
     Hasher hasher(8);
     Table t = Table(&hasher);
-    t.Insert(1);
-    assert(!t.Remove(2));
-    assert(t.Contains(1));
+    t.Insert("1");
+    assert(!t.Remove("2"));
+    assert(t.Contains("1"));
     std::cout << "PASS test_Table__InsertedOneElement__UnsuccessfulDeletionOfNonExistingElement" << std::endl;
 }
 
 void test_Table__InsertedTwoElements__DeletesBoth() {
     Hasher hasher(8);
     Table t = Table(&hasher);
-    t.Insert(1);
-    t.Insert(2);
-    assert(t.Remove(1));
-    assert(t.Remove(2));
-    assert(!t.Contains(1));
-    assert(!t.Contains(2));
+    t.Insert("1");
+    t.Insert("2");
+    assert(t.Remove("1"));
+    assert(t.Remove("2"));
+    assert(!t.Contains("1"));
+    assert(!t.Contains("2"));
     std::cout << "PASS test_Table__InsertedTwoElements__DeletesBoth" << std::endl;
 }
