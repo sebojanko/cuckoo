30cef8ce76a4daaced0ccaeb5d369bb3c150a782
Sebastian Janko <janko.sebastian@gmail.com>
2018-12-17 20:05:15 +0100

    added performance tests for containmen

--- Makefile
+++ Makefile
@@ -5,4 +5,4 @@ test:
 gen:
 	g++ -std=c++17 src/KMerGenerator/main.cpp src/FastADocument.cpp src/KMerGenerator/KMerGenerator.cpp	 -o kmergen
 bench:
-	g++ -std=c++17 src/benchmarks/main.cpp src/Cuckoo.cpp src/Bucket.cpp src/FastADocument.cpp src/GeneSequence.cpp src/Hash.cpp src/Table.cpp -o benchmark -lcrypto -lssl
+	g++ -O3 -std=c++17 src/benchmarks/main.cpp src/Cuckoo.cpp src/Bucket.cpp src/FastADocument.cpp src/GeneSequence.cpp src/Hash.cpp src/Table.cpp -o benchmark -lcrypto -lssl
--- src/benchmarks/main.cpp
+++ src/benchmarks/main.cpp
@@ -1,5 +1,5 @@
 //
-// Created by sebo on 10/22/18.
+// Created by sebo on 12/16/18.
 //
 #include <iostream>
 #include "../Cuckoo.h"
@@ -21,15 +21,51 @@ int main(int argc, const char* argv[]) {
 
     Cuckoo c = Cuckoo();
 
-    clock_t begin = clock();
+    std::cout << "Inserting 1M elems" << std::endl;
 
+    clock_t begin = clock();
     for (int i = 0; i < 1000000; i++) {
         c.Insert(i);
     }
-
     clock_t end = clock();
     double elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
     std::cout << "Time to insert 1M elems (our impl):" << elapsed_secs << std::endl;
+    
+    std::cout << "Checking 50000 inserted elements:" << std::endl;
+    int found{};
+    int not_found{};
+    begin = clock();
+    for (int i = 0; i < 500000; i++) {
+        if (c.Contains(i)) {
+            found++;
+        } else {
+            not_found++;
+        }
+    }
+
+    end = clock();
+    elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "Time to check existing 500000 elems (our impl):" << elapsed_secs << std::endl;
+    std::cout << "Found - " << found << std::endl;
+    std::cout << "Not found - " << not_found << std::endl;
+
+    std::cout << "Checking 50000 not inserted elements:" << std::endl;
+    found = 0;
+    not_found = 0;
+    begin = clock();
+    for (int i = 1'000'000; i < 1500000; i++) {
+        if (c.Contains(i)) {
+            found++;
+        } else {
+            not_found++;
+        }
+    }
+
+    end = clock();
+    elapsed_secs = double(end - begin) / CLOCKS_PER_SEC;
+    std::cout << "Time to check non existing 500000 elems (our impl):" << elapsed_secs << std::endl;
+    std::cout << "Found - " << found << std::endl;
+    std::cout << "Not found - " << not_found << std::endl;
 
     return 0;
 }
