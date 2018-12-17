86aac28f919becd213420611a597608083c7838c
Luka Markusic <markusicluka@gmail.com>
2018-12-17 22:35:22 +0100

    Add std::list

--- src/Table.cpp
+++ src/Table.cpp
@@ -1,48 +1,59 @@
 //
-// Created by sebo on 10/22/18.
+// Created by Luka on 10/22/18.
 //
 
 #include <iostream>
 #include "Table.h"
 
-Table::Table(Hasher *hasher) {
+// čisto za debug
+template <class T>
+void print(std::list<T> v) {
+    for (auto val : v) {
+        std::cout << val << " ";
+    }
+}
+
+Table::Table(Hasher *hasher, int b_size) {
     hasher_ = hasher;
+    bucket_size = b_size;
     //table_ = {};
 }
 
 void Table::Insert(std::string element) {
-    size_t h = getHash(element);
-    if (table_.find(h) != table_.end()) {
-        table_[h].Push(element);
-    } else {
-        table_[h] = Bucket();
-        table_[h].Push(element);
+    int iter = 0;
+    int h = getHash(element);
+    while (iter++ < 500) {
+        if (table_[h].size() < 8) {
+            table_[getHash(element)].push_front(getFingerprint(element));
+            return;
+        } else {
+            //magic
+        }
     }
 }
 
 bool Table::Contains(std::string element) {
     size_t h = getHash(element);
-    if (table_.find(h) != table_.end()) {
-        return table_[h].Contains(element);
-    }
-    return false;
+    return std::find(table_[h].begin(), table_[h].end(), getFingerprint(element)) != table_[h].end();
 }
 
 bool Table::Remove(std::string element) {
     size_t h = getHash(element);
-    if (table_.find(h) != table_.end()) {
-        return table_[h].Remove(element);
-    }
-    return false;
+    table_[h].erase(std::find(table_[h].begin(), table_[h].end(), element));
+    // trebam vidjeti kaj vraca
+    return true;
 }
 
 void Table::Print() {
-    for (auto v : table_) {
-        v.second.print();
+    for (auto [k, v] : table_) {
+        print(v);
         std::cout << std::endl;
     }
 };
 
+int Table::getFingerprint(std::string element) {
+    return hasher_->fingerprint(element);
+}
 int Table::getHash(std::string element) {
     return hasher_->hash(element);
 }
--- src/Table.h
+++ src/Table.h
@@ -1,5 +1,5 @@
 //
-// Created by sebo on 10/22/18.
+// Created by Luka on 10/22/18.
 //
 
 #ifndef CUCKOO_TABLE_H
@@ -7,7 +7,8 @@
 
 #include <array>
 #include <map>
-#include "Bucket.h"
+#include <list>
+#include <algorithm>
 #include "Hash.h"
 
 class Table {
@@ -15,20 +16,23 @@ private:
     Hasher *hasher_;
     // TODO trebat ce ipak ovdje ubaciti 2D polje izgleda
     //std::map<std::string, Bucket*> table_;
-    std::map<int, Bucket> table_;
+    std::map<int, std::list<int>> table_;
     //static const int ROWS{1000};
     //static const int COLS{1000};
     //int table_[ROWS][COLS]{};
+    int bucket_size;
     int getHash(std::string element);
+    int getFingerprint(std::string element);
 
 public:
-    Table(Hasher *hasher);
+    Table(Hasher *hasher, int bucket_size = 8);
 
     void Insert(std::string element);
 
     bool Remove(std::string element);
 
     bool Contains(std::string element);
+
 
     void Print();
 
