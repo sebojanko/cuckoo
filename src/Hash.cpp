86871f61b888c5977b5696f15e8caed647a559d2
Luka Markusic <markusicluka@gmail.com>
2018-12-28 22:52:15 +0100

    Revert hash sources

--- src/Hash.cpp
+++ src/Hash.cpp
@@ -18,8 +18,7 @@ Hasher::Hasher(size_t bits_per_item, Hash hash_function) {
     this->hash_function_ = hash_function;
 }
 
-template<class T>
-std::uint64_t Hasher::hash(T& item) {
+std::uint64_t Hasher::hash(int item) {
     if (this->hash_function_ == Hash::TIMS) {
         return twoIndependentMultiplyShift(item);
     }
@@ -46,16 +45,39 @@ std::uint64_t Hasher::hash(T& item) {
     }
 }
 
+std::uint64_t Hasher::hash(std::string item) {
+    if (this->hash_function_ == Hash::MD5) {
+        unsigned char md[MD5_DIGEST_LENGTH];
+        unsigned char *charData = (unsigned char*) item.c_str();
+        MD5(charData, item.length(), (unsigned char*)&md);
+        return stringToUint32(md);
+    }
+    if (this->hash_function_ == Hash::SHA1) {
+        unsigned char md[SHA_DIGEST_LENGTH];
+        unsigned char *charData = (unsigned char*) item.c_str();
+        SHA1(charData, item.length(), (unsigned char*)&md);
+        return stringToUint32(md);
+    }
+    if (this->hash_function_ == Hash::STL) {
+        return std::hash<std::string>{}(item);
+    }
+}
+
 std::uint64_t Hasher::operator()(std::string item) {
     return std::hash<std::string>{}(item);
 }
 
-template<class T>
-std::uint16_t Hasher::fingerprint(T& item) {
+std::uint16_t Hasher::fingerprint(std::string item) {
     uint64_t fp = hash(item);
     fp = fp & ((1ULL << bits_per_item_) - 1);
     fp += (fp == 0);
     return fp;
+}
+
+std::uint16_t Hasher::fingerprint(uint64_t hash) {
+    hash = hash & ((1ULL << bits_per_item_) - 1);
+    hash += (hash == 0);
+    return hash;
 }
 
 uint32_t stringToUint32(unsigned char *data) {
