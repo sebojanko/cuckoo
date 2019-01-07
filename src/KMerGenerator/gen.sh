6107d76df2a0c4b4cf688f82adc0916f4340685c
Sebastian Janko <janko.sebastian@gmail.com>
2019-01-07 17:53:04 +0100

    added shell script to generate input files

---
+++ src/KMerGenerator/gen.sh
@@ -0,0 +1,59 @@
+./../../kmergen 10 500 generated_10mer_500.txt gen
+./../../kmergen 10 5000 generated_10mer_5000.txt gen
+./../../kmergen 10 50000 generated_10mer_50000.txt gen
+./../../kmergen 10 500000 generated_10mer_500000.txt gen
+./../../kmergen 10 1000000 generated_10mer_1000000.txt gen
+
+./../../kmergen 20 500 generated_20mer_500.txt gen
+./../../kmergen 20 5000 generated_20mer_5000.txt gen
+./../../kmergen 20 50000 generated_20mer_50000.txt gen
+./../../kmergen 20 500000 generated_20mer_500000.txt gen
+./../../kmergen 20 1000000 generated_20mer_1000000.txt gen
+
+./../../kmergen 50 500 generated_50mer_500.txt gen
+./../../kmergen 50 5000 generated_50mer_5000.txt gen
+./../../kmergen 50 50000 generated_50mer_50000.txt gen
+./../../kmergen 50 500000 generated_50mer_500000.txt gen
+./../../kmergen 50 1000000 generated_50mer_1000000.txt gen
+
+./../../kmergen 100 500 generated_100mer_500.txt gen
+./../../kmergen 100 5000 generated_100mer_5000.txt gen
+./../../kmergen 100 50000 generated_100mer_50000.txt gen
+./../../kmergen 100 500000 generated_100mer_500000.txt gen
+./../../kmergen 100 1000000 generated_100mer_1000000.txt gen
+
+./../../kmergen 200 500 generated_200mer_500.txt gen
+./../../kmergen 200 5000 generated_200mer_5000.txt gen
+./../../kmergen 200 50000 generated_200mer_50000.txt gen
+./../../kmergen 200 500000 generated_200mer_500000.txt gen
+./../../kmergen 200 1000000 generated_200mer_1000000.txt gen
+
+./../../kmergen 10 500 extracted_10mer_500.txt ext ../../ecoli.fa
+./../../kmergen 10 5000 extracted_10mer_5000.txt ext ../../ecoli.fa
+./../../kmergen 10 50000 extracted_10mer_50000.txt ext ../../ecoli.fa
+./../../kmergen 10 500000 extracted_10mer_500000.txt ext ../../ecoli.fa
+./../../kmergen 10 1000000 extracted_10mer_1000000.txt ext ../../ecoli.fa
+
+./../../kmergen 20 500 extracted_20mer_500.txt ext ../../ecoli.fa
+./../../kmergen 20 5000 extracted_20mer_5000.txt ext ../../ecoli.fa
+./../../kmergen 20 50000 extracted_20mer_50000.txt ext ../../ecoli.fa
+./../../kmergen 20 500000 extracted_20mer_500000.txt ext ../../ecoli.fa
+./../../kmergen 20 1000000 extracted_20mer_1000000.txt ext ../../ecoli.fa
+
+./../../kmergen 50 500 extracted_50mer_500.txt ext ../../ecoli.fa
+./../../kmergen 50 5000 extracted_50mer_5000.txt ext ../../ecoli.fa
+./../../kmergen 50 50000 extracted_50mer_50000.txt ext ../../ecoli.fa
+./../../kmergen 50 500000 extracted_50mer_500000.txt ext ../../ecoli.fa
+./../../kmergen 50 1000000 extracted_50mer_1000000.txt ext ../../ecoli.fa
+
+./../../kmergen 100 500 extracted_100mer_500.txt ext ../../ecoli.fa
+./../../kmergen 100 5000 extracted_100mer_5000.txt ext ../../ecoli.fa
+./../../kmergen 100 50000 extracted_100mer_50000.txt ext ../../ecoli.fa
+./../../kmergen 100 500000 extracted_100mer_500000.txt ext ../../ecoli.fa
+./../../kmergen 100 1000000 extracted_100mer_1000000.txt ext ../../ecoli.fa
+
+./../../kmergen 200 500 extracted_200mer_500.txt ext ../../ecoli.fa
+./../../kmergen 200 5000 extracted_200mer_5000.txt ext ../../ecoli.fa
+./../../kmergen 200 50000 extracted_200mer_50000.txt ext ../../ecoli.fa
+./../../kmergen 200 500000 extracted_200mer_500000.txt ext ../../ecoli.fa
+./../../kmergen 200 1000000 extracted_200mer_1000000.txt ext ../../ecoli.fa
